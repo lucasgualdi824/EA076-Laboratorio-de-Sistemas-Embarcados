dff3fb45596746481cfe4906df807cebeb40cc96
lucasgualdi824 <lucas.gualdi4@gmail.com>
2024-11-28 19:35:20 -0300

    Update server.py

--- PulseGuard - v2/server.py
+++ PulseGuard - v2/server.py
@@ -16,4 +16,4 @@ def exibir_dados():
     return jsonify(dados)  # Retorna os dados coletados em formato JSON
 
 if __name__ == '__main__':
-    app.run(host='172.20.10.3', port=5000)
+    app.run(host='000.00.00.0', port=0000)
